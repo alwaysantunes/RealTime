33a43bbec1b431eb1e67098ac36a2d18cb5e5253
Tiago Antunes <antunestiago2004@gmail.com>
2024-12-13 22:25:37 +0000

    signup complee

--- back-end/src/contollers/auth.controller.js
+++ back-end/src/contollers/auth.controller.js
@@ -5,13 +5,18 @@ import bcrypt from "bcryptjs"
 export const signup = async (req, res) => {
     const{fullName,email,password} = req.body 
     try {
-        if (password.lenght < 6){
+
+        if (!fullName|| !email || !password){
+            return res.status(400).json({message: "all fields must be filled"});
+        }
+
+        if (password.length < 6){
             return res.status(400).json({message: "Password must be at least 6 characters"});
         }
 
         const user = await User.findOne({email}) 
 
-        if (User) return res.status(400).json({message: "email already exists"})
+        if (user) return res.status(400).json({message: "email already exists"})
 
 
        const salt = await bcrypt.genSalt(10) 
@@ -41,7 +46,7 @@ export const signup = async (req, res) => {
 
     } catch (error) {
         console.log("error in signup controller", error.message);
-        res.status(500).json({message:"internal sever error"})
+        res.status(500).json({message:"Internal sever error"})
 
     }
 }
--- back-end/src/index.js
+++ back-end/src/index.js
@@ -5,11 +5,11 @@ import { connectDB } from "./lib/db.js"
 
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
