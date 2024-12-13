3927874ff26b20b5f0af23e9be96c67fee0883fa
Tiago Antunes <antunestiago2004@gmail.com>
2024-12-13 05:47:30 +0000

    more sign up

--- back-end/src/contollers/auth.controller.js
+++ back-end/src/contollers/auth.controller.js
@@ -1,12 +1,47 @@
-export const signup = (req, res) => {
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+export const signup = async (req, res) => {
     const{fullName,email,password} = req.body 
     try {
         if (password.lenght < 6){
             return res.status(400).json({message: "Password must be at least 6 characters"});
         }
 
+        const user = await User.findOne({email}) 
+
+        if (User) return res.status(400).json({message: "email already exists"})
+
+
+       const salt = await bcrypt.genSalt(10) 
+       const hashedPassword = await bcrypt.hash(password,salt)   
+
+       const newUser = new User ({
+        fullName,
+        email,
+        password: hashedPassword,
+       })
+
+       if (newUser){
+        //jwt token
+        generateToken(newUser._id,res)
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic,
+        });
+
+       } else {
+        res.status(400).json({message: "invalid user data"})
+       }
 
     } catch (error) {
+        console.log("error in signup controller", error.message);
+        res.status(500).json({message:"internal sever error"})
 
     }
 }
