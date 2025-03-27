c915769a52616e130616a333dfdc994848bce67e
Tiago Antunes <antunestiago2004@gmail.com>
2025-03-27 23:57:02 +0000

    update

--- back-end/src/contollers/message.contoller.js
+++ back-end/src/contollers/message.contoller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js"
+import cloudinary from "cloudinary"
 
 export const getUsersForSidebar = async (req,res) => {
     try{
@@ -12,4 +14,68 @@ export const getUsersForSidebar = async (req,res) => {
         res.status(500).json({error: "Internal servor error"});
 
     }
+};
+
+export const getMessages = async(req,res) => {
+
+    try{
+        const { id:userToChatId } = req.params
+        const senderId = req.user.id;
+
+        const messages = await Message.find({
+            $or:[
+                {senderId:senderId,recieverId:userToChatId},
+                {senderId:userToChatId,recieverId:senderId}
+            ]
+        })
+
+        res.status(200).json(messages);
+
+
+    }catch (error){
+        console.log("Error uin getMessages; ", error.message);
+        res.status(500).json({error: "Internal servor error"});
+
+
+    };
+
+};
+
+export const sendMessage = async (req,res) => {
+
+    try{
+        const {text, image} = req.body;
+        const {id: recieverId} = req.params;
+        const senderId = req.user.id;
+
+        let imageUrl;
+        if (image){
+            //upload to cloudinary
+            const uploadResposne = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResposne.secure_url;
+
+        }
+
+        const newMessage = new Message({
+            senderId,
+            recieverId,
+            text,
+            image:imageUrl,
+        });
+
+        await newMessage.save();
+
+        // realtime(socket.io)
+
+
+        res.status(201).json(newMessage)
+
+
+    }catch(error){
+        console.log("Error in sendMessage; ", error.message);
+        res.status(500).json({error: "Internal servor error"});
+
+
+    };
+
 };
--- back-end/src/routes/message.routes.js
+++ back-end/src/routes/message.routes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {getUsersForSidebar} from "../contollers/message.contoller.js";
+import { getMessages } from "../contollers/message.contoller.js";
+import { sendMessage } from "../contollers/message.contoller.js";
 
-
-const router = express.router(); 
+const router = express.Router(); 
 
 router.get("/user",protectRoute, getUsersForSidebar);
+router.get("/:id",protectRoute, getMessages);
 
+router.post("/send/:id", protectRoute, sendMessage);
 
 
 export default router;
